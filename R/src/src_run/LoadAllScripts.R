52239f6822c09e4d2e7bdac0c549b5f4ffd51290
evah000 <eva.h11251018@gmail.com>
2025-05-08 18:27:43 -0400

    Delete R directory

--- R/src/src_run/LoadAllScripts.R
+++
@@ -1,6 +0,0 @@
-#-------------------------------------------------------------------------------
-LoadAllScripts <- function(src.path = "~/Dropbox/CHUKNORRIS/R/src/"){
-  all.scripts <- list.files(src.path, pattern = "[.]R$", recursive = TRUE)
-  invisible(lapply(paste(src.path, all.scripts, sep = ""), source))
-}
-#-------------------------------------------------------------------------------
--- R/src/src_ts/SamplingIntervalFuns.R
+++
@@ -1,9 +0,0 @@
-#-------------------------------------------------------------------------------
-GetSamplingIntervalMode <- function(t.vec, dg = 4){
-  return(Mode(round(diff(t.vec), digits = dg)))
-}
-#-------------------------------------------------------------------------------
-TestSamplingIntervalHomogeneity <- function(t.vec, dg = 6){
-  return(round(sd(round(diff(t.vec), digits = dg)), digits = dg) == 0)
-}
-#-------------------------------------------------------------------------------
